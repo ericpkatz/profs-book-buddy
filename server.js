aae2b67bb720070dd59b5f588385ff7a3dfe9e7e
ericpkatz <ericpkatz@gmail.com>
2024-01-25 11:02:37 -0500

    starting with database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/profs_book_buddy');
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -13,6 +16,12 @@ app.get('/', (req, res, next)=> {
   res.send('<html><body><h1>Hello World</h1></body></html>');
 });
 
-const port = process.env.PORT || 3000;
+const init = async()=> {
+  await client.connect();
+  console.log('connected to database');
+  const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+  app.listen(port, ()=> console.log(`listening on port ${port}`));
+}
+
+init();
